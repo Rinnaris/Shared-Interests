7545217c86a75c6239d94d3aa860eb6ea6d8408c
rochea <roche.alyssa7@gmail.com>
2017-02-22 22:11:17 -0400

    Started a Setup Activity
    Some GUI stuff for when the app would launch and there is no current
    profile. Welcomes the user and get them to enter their name. Currently I
    have not added any actual functionality, aside from making some things
    visible/invisible on startButton click. I haven't tested this yet, but I
    just wanted to get some UI stuff in there. It isn't really relevant to
    the demonstration on Feb.22

---
+++ app/src/main/java/com/example/wifidirecthandler/Setup.java
@@ -0,0 +1,45 @@
+package com.example.wifidirecthandler;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+//When there is no current profile, launch set-up, to begin the setting up of the profile?
+public class Setup extends AppCompatActivity {
+
+    Button startButton;
+    TextView instructionsText;
+    TextView welcomeText;
+    TextView skipText;
+    EditText nameField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.CYAN);
+
+        instructionsText = (TextView) findViewById(R.id.textInstructions);
+        welcomeText = (TextView) findViewById(R.id.textWelcome);
+        skipText = (TextView) findViewById(R.id.textSkip);
+        nameField = (EditText) findViewById(R.id.textName);
+
+        startButton = (Button) findViewById(R.id.buttonStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                welcomeText.setVisibility(View.INVISIBLE);
+                nameField.setVisibility(View.VISIBLE);
+                instructionsText.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
