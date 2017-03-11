f7ba254bce99adf30ef7528d111e7f9ea4c249d3
Rinnaris <rinnaris@Arias-MacBook-Pro.local>
2017-03-11 13:10:59 -0400

    Revert "Merge branch 'master' into aria"
    This reverts commit bc70326902aefada16cca3b2618b483904ce843d, reversing
    changes made to 71e3546333c341c1ddbefcb122b7499ead930038.

--- app/src/main/java/com/example/wifidirecthandler/Setup.java
+++ app/src/main/java/com/example/wifidirecthandler/Setup.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-<<<<<<< HEAD
 //When there is no current profile, launch set-up, to begin the setting up of the profile?
 public class Setup extends AppCompatActivity {
 
@@ -19,37 +18,6 @@ public class Setup extends AppCompatActivity {
     TextView welcomeText;
     TextView skipText;
     EditText nameField;
-=======
-import java.util.ArrayList;
-
-//When there is no current profile, launch set-up, to begin the setting up of the profile?
-public class Setup extends AppCompatActivity {
-
-    ArrayList<String> movie = new ArrayList<String>();
-    ArrayList<String> music = new ArrayList<String>();
-    ArrayList<String> book = new ArrayList<String>();
-    ArrayList<String> sport = new ArrayList<String>();
-    ArrayList<String> hobby = new ArrayList<String>();
-
-    Button movieButton;
-    Button musicButton;
-    Button bookButton;
-    Button sportButton;
-    Button hobbyButton;
-
-    EditText movieField;
-    EditText musicField;
-    EditText bookField;
-    EditText sportField;
-    EditText hobbyField;
-
-    TextView movieView;
-    TextView musicView;
-    TextView bookView;
-    TextView sportView;
-    TextView hobbyView;
-
->>>>>>> master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +26,6 @@ public class Setup extends AppCompatActivity {
         View view = this.getWindow().getDecorView();
         view.setBackgroundColor(Color.CYAN);
 
-<<<<<<< HEAD
         instructionsText = (TextView) findViewById(R.id.textInstructions);
         welcomeText = (TextView) findViewById(R.id.textWelcome);
         skipText = (TextView) findViewById(R.id.textSkip);
@@ -77,66 +44,4 @@ public class Setup extends AppCompatActivity {
             }
         });
     }
-=======
-        movieButton = (Button) findViewById(R.id.movieAdd);
-        musicButton = (Button) findViewById(R.id.musicAdd);
-        bookButton = (Button) findViewById(R.id.bookAdd);
-        sportButton = (Button) findViewById(R.id.sportAdd);
-        hobbyButton = (Button) findViewById(R.id.hobbyAdd);
-
-        movieField = (EditText) findViewById(R.id.newMovie);
-        musicField = (EditText) findViewById(R.id.newMusic);
-        bookField = (EditText) findViewById(R.id.newBook);
-        sportField = (EditText) findViewById(R.id.newSport);
-        hobbyField = (EditText) findViewById(R.id.newHobby);
-
-        movieView = (TextView) findViewById(R.id.movieList);
-        musicView = (TextView) findViewById(R.id.musicList);
-        bookView = (TextView) findViewById(R.id.bookList);
-        sportView = (TextView) findViewById(R.id.sportList);
-        hobbyView = (TextView) findViewById(R.id.hobbyList);
-
-
-        movieButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-           public void onClick(View v) {
-                movie.add(movieField.getText() + "");
-
-                for(int i = 0; i < movie.size(); i++) {
-                    String interest = movie.get(i);
-                    movieView.setText(movieView  + " " + interest + ", ");
-                }
-            }
-        });
-
-        musicButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                music.add(musicField.getText() + "");
-
-                for(int i = 0; i < music.size(); i++) {
-                    String interest = music.get(i);
-                    musicView.setText(musicView  + " " + interest + ", ");
-                }
-            }
-        });
-
-        bookButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                book.add(bookField.getText() + "");
-
-                for(int i = 0; i < book.size(); i++) {
-                    String interest = book.get(i);
-                    bookView.setText(bookView + " " + interest + ", ");
-                }
-            }
-        });
-    }
-
-
->>>>>>> master
 }
